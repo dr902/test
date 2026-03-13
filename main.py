1a6db02be5f1a324a517a482a852822c89b97211
hanumath-advanzinnovation <hanumath@advanzinnovation.com>
2026-03-13 13:15:01 +0530

    pushing backend

---
+++ main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello from API"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int):
+    return {"item_id": item_id}
+
+@app.post("/items/")
+def create_item(item: Item):
+    return {"message": "Item created successfully", "item": item}
