6095fb938ee09935463d8be9261f94381e596d34
dr902 <durga.g@tericsoft.com>
2026-03-16 11:14:13 +0530

    Modify paragraph and add image in App component
    Updated the paragraph text and added an image.

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,8 @@ function App() {
   return (
     <div className="App">
       <h1>Hello World from React!</h1>
-      <p>This is a sample React frontend application.</p>
+      <p>This is a sample React frontend application. Here is the updated one</p>
+      <img src="https://www.google.com/imgres?q=images&imgurl=https%3A%2F%2Fimg.freepik.com%2Ffree-photo%2Fcourage-man-jump-through-gap-hill-business-concept-idea_1323-262.jpg%3Fsemt%3Dais_hybrid%26w%3D740%26q%3D80&imgrefurl=https%3A%2F%2Fwww.freepik.com%2Ffree-photos-vectors%2Fachieve&docid=ufS61tXGpZBydM&tbnid=2F771NtejnF3pM&vet=12ahUKEwiLx9H12qOTAxUsR2wGHT2bJxQQnPAOegUIggEQAQ..i&w=740&h=494&hcb=2&ved=2ahUKEwiLx9H12qOTAxUsR2wGHT2bJxQQnPAOegUIggEQAQ" ></img>
     </div>
   )
 }
