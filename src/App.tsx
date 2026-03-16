a4b6703a23034baac9b250df847ff3ddda72efbe
hanumath-advanzinnovation <hanumath@advanzinnovation.com>
2026-03-16 10:10:23 +0530

    UI

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World from React!</h1>
+      <p>This is a sample React frontend application.</p>
+    </div>
+  )
+}
+
+export default App
